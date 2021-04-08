f4b492c2e85d840a9948b7fda1cc51cc247437ae
tiagoandresvaz <tiagoandresvaz@gmail.com>
2021-04-08 16:39:56 -0300

    bump

--- lib/getDomain.ts
+++ lib/getDomain.ts
@@ -15,7 +15,7 @@ const ENV_VARS = [
   'ZEIT_URL',
   'DEPLOY_PRIME_URL',
   'DEPLOY_URL',
-  'https://tiagoandresvaz.github.io/covid19_scenarios/',
+  'URL',
 ]
 
 export function getenvFirst(vars: string[]) {
